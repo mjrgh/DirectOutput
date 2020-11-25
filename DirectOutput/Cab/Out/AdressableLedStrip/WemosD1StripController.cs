66094903980276d9eeb1120d3b106f17627b91f3
VrôÕnsh <vroonsh@free.fr>
2020-11-25 17:45:21 +0100

    WemosD1MPStripController : added a new command to change the brightness of a ledstrip
    - new tag in controller desciptor PerLedstripBrightness
    - into this tag, set brightness values [0-255] separated by spaces, value index will be sent to the ledstrip on the same index (order declaration of <LedStrip> descriptors)
    - command sent 'B', led index (byte) , last led index (byte), brightness (byte)

--- DirectOutput/Cab/Out/AdressableLedStrip/WemosD1StripController.cs
+++ DirectOutput/Cab/Out/AdressableLedStrip/WemosD1StripController.cs
@@ -28,15 +28,26 @@ namespace DirectOutput.Cab.Out.AdressableLedStrip
         /// <value>
         /// true if the commands are sent
         /// </value>
-        /// <remarks>
-        /// These commands are supported by the new Wemos firmware from yoyofr & aetios50, supporting dynamic ledstrip length setup (part of an overall performance improvement)
-        /// </remarks>
         public bool SendPerLedstripLength
         {
             get { return _SendPerLedstripLength; }
             set {
                 _SendPerLedstripLength = value;
             }
+        }
+
+        private string _PerLedstripBrightness = string.Empty;
+
+        /// <summary>
+        /// Set the brightness per ledstrip in the range 0->255
+        /// </summary>
+        /// <value>
+        /// a string containing brightness values separated by spaces
+        /// </value>
+        public string PerLedstripBrightness
+        {
+            get { return _PerLedstripBrightness; }
+            set { _PerLedstripBrightness = value; }
         }
 
         protected override void SetupController()
@@ -53,6 +64,7 @@ namespace DirectOutput.Cab.Out.AdressableLedStrip
                     int nbleds = NumberOfLedsPerStrip[numled];
                     if (nbleds > 0) {
                         CommandData = new byte[5] { (byte)'Z', (byte)numled, (byte)(NumberOfLedsPerStrip.Length - 1), (byte)(nbleds >> 8), (byte)(nbleds & 255) };
+                        Log.Debug($"Resize ledstrip {numled} to {nbleds} leds.");
                         ComPort.Write(CommandData, 0, 5);
                         ReceiveData = new byte[1];
                         BytesRead = -1;
@@ -65,6 +77,34 @@ namespace DirectOutput.Cab.Out.AdressableLedStrip
                         if (BytesRead != 1 || ReceiveData[0] != (byte)'A') {
                             throw new Exception($"Expected a Ack (A) after setting the number of leds for ledstrip {numled}, but received no answer or a unexpected answer ({(char)ReceiveData[0]}). Will not send data to the controller.");
                         }
+                    }
+                }
+            }
+
+            //Send brightness per ledstrip if available
+            if (!PerLedstripBrightness.IsNullOrEmpty()) {
+                var values = PerLedstripBrightness.Split(' ');
+                var minlen = Math.Min(NumberOfLedsPerStrip.Length, values.Length);
+                for (var numled = 0; numled < minlen; ++numled) {
+                    var brightness = 0;
+                    try {
+                        brightness = Int32.Parse(values[numled]).Limit(0, 255);
+                    } catch (Exception E) {
+                        throw new Exception($"Cannot parse brigthness value for ledstrip {numled}, check if there are only [0-255] ranged numbers separated by spaces.", E);
+                    }
+                    CommandData = new byte[4] { (byte)'B', (byte)numled, (byte)(minlen - 1), (byte)(brightness) };
+                    Log.Debug($"Send brightness {brightness} for ledstrip {numled} [{string.Join(" ", CommandData)}].");
+                    ComPort.Write(CommandData, 0, 4);
+                    ReceiveData = new byte[1];
+                    BytesRead = -1;
+                    try {
+                        BytesRead = ReadPortWait(ReceiveData, 0, 1);
+                    } catch (Exception E) {
+                        throw new Exception($"Expected 1 bytes after setting the brightness for ledstrip {numled} , but the read operation resulted in a exception. Will not send data to the controller.", E);
+                    }
+
+                    if (BytesRead != 1 || ReceiveData[0] != (byte)'A') {
+                        throw new Exception($"Expected a Ack (A) after setting the brightness for ledstrip {numled}, but received no answer or a unexpected answer ({(char)ReceiveData[0]}). Will not send data to the controller.");
                     }
                 }
             }
